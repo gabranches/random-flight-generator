c0da2afd051071be95cd9f8101bcd2a933bca065
unknown <gabranches@gmail.com>
2020-09-17 18:20:25 -0400

    First commit

--- src/ModuleFactory.ts
+++
@@ -1,5 +0,0 @@
-export class ModuleFactory {
-  constructor() {
-    
-  }
-}
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,5 @@
-#!/usr/bin/env node
-import readline from 'readline';
-import { Reader } from './Reader';
-
-const prompt = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-const run = () => {
-  prompt.question('Ready.\n', async (cmd: string) => {
-    if (cmd === 'quit') {
-      prompt.close();
-    }
-    const response = await Reader.read(cmd);
-    console.log('\n')
-    response.forEach((line): void => {
-      console.log(line);
-    });
-    console.log('\n')
-    run();
-  });
+const sum = function (a: number, b: number) {
+  return a + b;
 };
 
-prompt.on('close', function () {
-  console.log('\nBYE BYE !!!');
-  process.exit(0);
-});
-
-run();
+export { sum };
